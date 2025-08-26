ab97194c4bb3b7277668e070971a9c798b3e8314
Udit Mishra <gyaneshmishra97@gmail.com>
2025-08-26 23:07:12 +0530

    Metadata and Controller Changes

--- 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -7,23 +7,25 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
-	
-	private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
-	
-	@Autowired
-	private CurrencyExchangeRepository repository;
-	
-	@Autowired
-	private Environment environment;
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
